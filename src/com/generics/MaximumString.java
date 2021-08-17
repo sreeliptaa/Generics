c595c3e1be381c1a180ec5806ec085e02963a6ab
sreeliptaa <sreelipta65@gmail.com>
2021-08-18 00:32:14 +0530

    Added: finding the maximum from three string

--- src/com/generics/MaximumString.java
+++ src/com/generics/MaximumString.java
@@ -1,36 +1,35 @@
 package com.generics;
 /**
- * Program to find the maximum of three float
+ * Program to find the maximum of three String
  * @author Sreelipta
  * @since 2021-08-17
  */
-public class MaximumFloat {
+public class MaximumString {
     /**
      * In main method
      * variables are declare and initialise
      */
     public static void main(String[] args) {
         //declaring varaibles
-        Float a = 2.7f;
-        Float b = 4.3f;
-        Float c = 3.2f;
+        String a = "apple";
+        String b = "peach";
+        String c = "banana";
         //calling method to main
         findMaximumNo(a,b,c);
     }
 
     /**
-     * Method for finding maximum number from given three integer parameters using compare to
-     * @param a,b,c
+     * Method for finding maximum number from given three string using compare to
+     * @param  a, b, c
      */
-    private static void findMaximumNo(Float a, Float b, Float c) {
-        Float max = a;
+    private static void findMaximumNo(String a, String b, String c) {
+        String max = a;
         if (b.compareTo(max)>0) {
             max = b;
         }
         if (c.compareTo(max)>0) {
             max = c;
         }
-        System.out.println("Maximum number of given three no is " +max);
+        System.out.println("Maximum String of given three is " +max);
     }
 }
-
