786fccddea2199f39f8174e139a50c89f6c9df8b
sreeliptaa <sreelipta65@gmail.com>
2021-08-19 13:57:30 +0530

    Added: refactor all three to one generic method to find max value

---
+++ src/com/generics/MaximumValue.java
@@ -0,0 +1,33 @@
+package com.generics;
+/**
+ * Program to find the maximum of three String
+ * @author Sreelipta
+ * @since 2021-08-17
+ */
+public class MaximumValue {
+
+    /**
+     * Generic method to compare any type of data and find maximum
+     * @param  x1, x2, x3
+     */
+    public static <T extends Comparable<T>> T findMaxValues(T x1, T x2, T x3) {
+        T max = x1;
+        if (x2.compareTo(max) > 0)
+            max = x2;
+        if (x3.compareTo(max) > 0)
+            max = x3;
+        return max;
+    }
+    /**
+     * In main method
+     * variables are declare and initialise
+     */
+    public static void main(String[] args) {
+        Integer a1 = 66, a2 = 45, a3 = 98;
+        System.out.println("The Maximum between three integers is : " + findMaxValues(a1, a2, a3));
+        Float f1 = 66.21f, f2 = 45.76f, f3 = 98.43f;
+        System.out.println("The Maximum between three float is : " + findMaxValues(f1, f2, f3));
+        String s1 = "apple", s2 = "banana", s3 = "peach";
+        System.out.println("The Maximum between three string is : " + findMaxValues(s1, s2, s3));
+    }
+}
