935fd889917aab9801c0431f884ecc8efc332d84
sreeliptaa <sreelipta65@gmail.com>
2021-08-19 15:09:23 +0530

    Added: find max value of more than three parameters

--- src/com/generics/MaximumValue.java
+++ src/com/generics/MaximumValue.java
@@ -1,40 +1,38 @@
 package com.generics;
+
+import java.util.Arrays;
+
 /**
- * Program to find the maximum
+ * Program to find the maximum using generics
  * @author Sreelipta
  * @since 2021-08-17
  */
 public class MaximumValue<T extends Comparable<T>> {
-    T x1, x2, x3;
 
-    public MaximumValue(T x1, T x2, T x3) {
-        this.x1 = x1;
-        this.x2 = x2;
-        this.x3 = x3;
+    T[] elements;
+
+    public MaximumValue(T[] elements) {
+        this.elements = elements;
     }
-    
+
     /**
-     * Generic method to compare any type of data and find maximum
-     * @param  x1, x2, x3
+     * Generic method to store values in array and return maximum value.
+     * @param elements
+     * @return max : Maximum of three values
      */
-    public static <T extends Comparable<T>> T findMaxValues(T x1, T x2, T x3) {
-        T max = x1;
-        if (x2.compareTo(max) > 0)
-            max = x2;
-        if (x3.compareTo(max) > 0)
-            max = x3;
+    public static <T extends Comparable<T>> T findMaxValues(T[] elements) {
+        Arrays.sort(elements);
+        int lenght = elements.length;
+        T max = elements[lenght - 1];
         return max;
     }
-    /**
-     * In main method
-     * variables are declare and initialise
-     */
+
     public static void main(String[] args) {
-        Integer a1 = 66, a2 = 45, a3 = 98;
-        System.out.println("The Maximum between three integers is : " + findMaxValues(a1, a2, a3));
-        Float f1 = 66.21f, f2 = 45.76f, f3 = 98.43f;
-        System.out.println("The Maximum between three float is : " + findMaxValues(f1, f2, f3));
-        String s1 = "apple", s2 = "banana", s3 = "peach";
-        System.out.println("The Maximum between three string is : " + findMaxValues(s1, s2, s3));
+        Integer[] intMax = { 23, 32, 87, 90, 82, 76, 19, 48 };
+        System.out.println("The Maximumnumber of these integers is : " + findMaxValues(intMax));
+        Float[] floatMax = { 2.3f, 3.2f, 8.77f, 69.7f, 4.56f, 66.87f };
+        System.out.println("The Maximum number of these float is : " + findMaxValues(floatMax));
+        String[] stringMax = { "pizza", "burger", "pasta", "cutlet", "pastry" };
+        System.out.println("The Maximum number of these String is : " + findMaxValues(stringMax));
     }
 }
